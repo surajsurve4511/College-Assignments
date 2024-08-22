9f7b9cb4e00c7c6c48b7d3e1deef369799086d3d
Suraj Prakash Surve <141995340+surajsurve4511@users.noreply.github.com>
2024-08-22 16:01:25 +0530

    Update 5_selection_sort.py

--- FDS/5_selection_sort.py
+++ FDS/5_selection_sort.py
@@ -7,3 +7,16 @@ def input_array(arr):
   return arr
 input_array(arr)
 print("The array is: ", arr)
+
+
+
+def selection_sort(arr):
+  count = 0
+  for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+      if arr[i]>arr[j]:
+        arr[i], arr[j] = arr[j], arr[i]
+      count+=1
+  print("The number of swaps are: ", count)
+  return arr
+print(selection_sort(arr))
