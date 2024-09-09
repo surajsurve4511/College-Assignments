bbc0d4c2a3ec167a2b511a8bd335c6ce6bf8b644
Suraj Prakash Surve <141995340+surajsurve4511@users.noreply.github.com>
2024-09-09 15:17:47 +0530

    Create SJF.cpp

---
+++ OS/SJF.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Process{
+    public:
+        int pid;
+        int bt;
+        int tat;
+        int wt;
+    
+    void getData(){
+    cout<<"Enter Pid : ";
+    cin>>this->pid;
+    cout<<"Enter Burst time";
+    cin>>this->bt;
+    this->wt=0;
+    this->tat=0;
+}
+};
+
+void Calculate (Process p[],int n){
+    p[0].tat = p[0].bt;
+    for(int i=1;i<n;i++){
+        p[i].tat = p[i-1].tat+p[i].bt;
+        p[i].wt = p[i].tat - p[i].bt;
+    }
+    
+}
+
+void sort(Process p[],int n){
+    
+    Process temp[n];
+    
+    for(int i = 0; i < n; i++) {
+    for(int j = i + 1; j < n; j++) {
+        if(p[j].bt < p[i].bt) {
+            // Swap p[i] and p[j]
+            Process temp = p[j];
+            p[j] = p[i];
+            p[i] = temp;
+        }
+    }
+}
+}
+
+int main(){
+  
+    int n;
+    cout<<"Enter the Number of Processes : ";
+    cin>>n;
+    
+    Process p[n];
+    for(int i=0;i<n;i++){
+        p[i]=Process();
+        p[i].getData();
+    }
+    sort(p,n);
+    Calculate(p,n);
+    
+    cout<<"pid\t"<<"bt\t"<<"tat\t"<<"wt\t"<<endl;
+    
+    for(int i = 0;i<n;i++){
+        cout<<p[i].pid<<"\t"<<p[i].bt<<"\t"<<p[i].tat<<"\t"<<p[i].wt<<endl;
+    
+    }
+  
+    return 0;
+}
